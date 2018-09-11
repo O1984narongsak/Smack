6078908fb02c07d7864f90b95fec9ec8d1e07bcc
Narongsak Launwijit <censpi@gmail.com>
2018-09-12 00:11:59 +0700

    Design Login

---
+++ Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Narongsak_O on 11/9/18.
+//  Copyright © 2018 nProject. All rights reserved.
+//
+
+import Foundation
+
+let TO_LOGIN = "toLogIn"
