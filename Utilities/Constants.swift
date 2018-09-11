ddffc341831cbb7465eba0dd0e236a0d074d9dbf
Narongsak Launwijit <censpi@gmail.com>
2018-09-12 02:47:21 +0700

    Adding Service (with out DB)

--- Utilities/Constants.swift
+++ Utilities/Constants.swift
@@ -8,6 +8,21 @@
 
 import Foundation
 
+typealias CompletionHandle = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = ""
+let URL_REGISTER = "\(BASE_URL)"
+
+// Seques
+
 let TO_LOGIN = "toLogIn"
 let TO_CREATE = "toCreate"
 let UNWIND = "unwindToChennel"
+
+// User Defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
