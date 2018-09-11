7088b9318bc967f932a0ade4fd5583f217061544
Narongsak Launwijit <censpi@gmail.com>
2018-09-11 22:44:50 +0700

    Add Table View & Design

---
+++ View/GradientView.swift
@@ -0,0 +1,36 @@
+//
+//  GradientView.swift
+//  Smack
+//
+//  Created by Narongsak_O on 11/9/18.
+//  Copyright © 2018 nProject. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+
+class GradientView: UIView {
+
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.1764705926, green: 0.4980392158, blue: 0.7568627596, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    @IBInspectable var botColor: UIColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1) {
+        didSet {
+            self.setNeedsLayout()
+        }
+    }
+    
+    override func layoutSubviews() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.colors = [topColor.cgColor,botColor.cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.frame = self.bounds
+        self.layer.insertSublayer(gradientLayer, at: 0)
+    }
+
+}
